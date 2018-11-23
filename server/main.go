48428ec708aae77a36307e32d47fba6997f16045
Mark Chmarny <mark@chmarny.com>
2018-11-23 09:56:58 -0800

    More refactoring

--- server/main.go
+++ server/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/mchmarny/github-activity-counter/handler"
 )
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, fmt.Sprintf("{'url': '%s'}", r.URL.Path[1:]))
+}
 
 func main() {
 
@@ -16,6 +23,7 @@ func main() {
 		port = "8080"
 	}
 
+	http.HandleFunc("/", defaultHandler)
 	http.HandleFunc("/events", handler.GitHubEventHandler)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
